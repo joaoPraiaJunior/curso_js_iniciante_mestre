e6c414688d9819ffc7a197b1c21fefc04ee3ca35
Joao Praia <joaopraia@joaopraia.com.br>
2023-08-13 21:30:42 -0300

    aula funcoes

---
+++ funcoes/introducao.js
@@ -0,0 +1,31 @@
+/* Funcoes são objetos invocáveis';
+Podem ser literais;
+Em alguns casos ocorre hoisting;
+São objetos de primeira classe;
+No ES5 as funções são o único modo de gerar escopo;
+Podem ser IIFE (Immdiately, Invoked, Function Expression);
+Podem ter propriedades internas como arguments e name;
+*/
+
+// Funcoes declaradas
+
+function declarada() {
+  console.log("testando funcao");
+}
+
+// Hoisting: capacidade do javascript de 'içar' uma função declarada
+
+declaradaHoisting();
+
+function declaradaHoisting() {
+  console.log("testando funcao declarada Hoisting");
+}
+
+// o mesmo não aontece com funções de expressão
+
+const expressaoHoisting = function () {
+  console.log("testando funcao expressao Hoisting");
+}
+
+expressaoHoisting();
+
