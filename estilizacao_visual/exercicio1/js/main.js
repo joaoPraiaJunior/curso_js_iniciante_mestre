ef0def17d61ee22d0a697939146723c4732e098f
Joao Praia <joaopraia@joaopraia.com.br>
2023-09-08 17:22:41 -0300

    Exercicio 1 de estilização

---
+++ estilizacao_visual/exercicio1/js/main.js
@@ -0,0 +1,43 @@
+(function () {
+  "use strict";
+
+  const elementos = {
+    menuDeNavegacao: ".nav",
+    itensDeMenu: ".item-menu > a",
+  };
+
+  const menuDeNavegacao = document.querySelector(
+    `${elementos.menuDeNavegacao}`
+  );
+  const itensDeMenu = document.querySelectorAll(`${elementos.itensDeMenu}`);
+
+  itensDeMenu[0].classList.add("actived");
+
+  function destacaItensDoMenu() {
+    let posicoes = [...itensDeMenu].map((item) => {
+      let hash = item.getAttribute("href");
+      let selecionaConteudo = document.querySelector(hash);
+      let posicaoDoConteudo = selecionaConteudo.getBoundingClientRect().top;
+      return posicaoDoConteudo;
+    });
+
+    console.log(posicoes);
+
+    let itemDeMenuAtivo = menuDeNavegacao.querySelector(".actived");
+    if (itemDeMenuAtivo) itemDeMenuAtivo.classList.remove("actived");
+    let itemDeMenu = pegaUltimoElementoVisto(posicoes);
+    if (itemDeMenu) {
+      return itemDeMenu.classList.add("actived");
+    }
+
+    return itensDeMenu[0].classList.add("actived");
+  }
+
+  function pegaUltimoElementoVisto(posicoes) {
+    let posicaoAtual = posicoes.filter((posicao) => posicao < innerHeight / 2);
+    console.log(posicaoAtual);
+    console.log(itensDeMenu[posicaoAtual.length - 1]);
+    return itensDeMenu[posicaoAtual.length - 1];
+  }
+  window.addEventListener("scroll", destacaItensDoMenu);
+})();
