3072fd24903b5686414d27ecf35f6d94ac1455e1
Joao Praia <joaopraia@joaopraia.com.br>
2023-09-17 22:46:50 -0300

    Atualização da Aula2 de POO

--- POO/POO2.js
+++ POO/POO2.js
@@ -36,5 +36,35 @@ class Galinha extends Animal {
 }
 
 let carijo = new Galinha("carijó");
-console.log(carijo)
+console.log(carijo);
 console.log(carijo.obterTipo());
+
+//Observações sobre o instanceof
+const numero = 10;
+const numero2 = new Number(10);
+console.log(numero2);
+
+//Valor primitivo não é instâncias de Number
+console.log(numero instanceof Number);
+
+//No caso do número2, ele é instância da função construtora Number
+console.log(numero2 instanceof Number);
+console.log(typeof numero2);
+
+//Mas os construtores serão Number
+console.log(numero.constructor);
+console.log(numero2.constructor);
+
+//Quando não é primitivo, as instâcias de arr1 e arr2 é Array
+const arr1 = [];
+const arr2 = new Array();
+console.log(arr1 instanceof Array);
+console.log(arr2 instanceof Array);
+
+//Da mesma maneira do Array, ocorre com RegExp()
+const regex1 = /a/g;
+const regex2 = new RegExp();
+console.log(regex1 instanceof RegExp);
+console.log(regex2 instanceof RegExp);
+
+//Você pode saber se um determinado dado é seu tipo utilizando o instanceof ou typeof
